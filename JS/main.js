eb0a8597819928fcae11d66ce4523f56a55e6078
rebs-code <jack.rebussi@gmail.com>
2023-11-06 16:09:44 +0100

    feat: script

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+"use strict"
+
+//ask for input
+const km = parseInt(prompt("Inserisci il numero di chilometri che desideri percorrere:"));
+const age = parseInt(prompt("Inserisci la tua età:"));
+//initialize const variables
+const priceKm = 0.21;
+const totalTicketPrice = km * priceKm;
+//apply discount if needed
+let discount = 0;
+if (age < 18) {
+    discount = totalTicketPrice * 0.20;
+} else if (age >= 65) {
+    discount = totalTicketPrice * 0.40;
+}
+//calculate final price with discount
+const finalTicketPrice = totalTicketPrice - discount;
+//print final price in console
+console.log(`Il prezzo finale del biglietto è: €${finalTicketPrice.toFixed(2)}`);
